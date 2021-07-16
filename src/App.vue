3559cb6067fedfa1b0b51cd9e99d80f8706e693a
sunqiwei <18800650372@163.com>
2021-07-16 14:40:20 +0800

    登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    666666666666
     <router-view />
   </div>
 </template>
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      num: 6,
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    },
+  },
+  created() {},
 };
 </script>
 
